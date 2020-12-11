009e74c2a5ae7248725925afcb26223b57e38ba5
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2020-12-11 18:08:14 +0530

    Update chatbot.pl

--- chatbot.pl
+++ chatbot.pl
@@ -1,4 +1,11 @@
-# symptoms of COVID - source: Google
+/**************************************************
+                  COVID-19 Chatbot
+        Created by Harshdip Singh for COMP4400
+**************************************************/
+
+/*
+ symptoms of COVID - source: Google
+*/
 symptom('fever').
 symptom('dry cough').
 symptom('tiredness').
@@ -15,6 +22,30 @@ symptom('chest pain').
 symptom('loss of speech').
 
 /*
+* Responses - valid responses a user can type
+*/
+valid_response('yes').
+valid_response('Yes').
+valid_response('yup').
+valid_response('Yup').
+valid_response('no').
+valid_response('No').
+valid_response('nope').
+valid_response('Nope').
+
+positive_response('Yes').
+positive_response('yes').
+positive_response('yup').
+positive_response('Yup').
+
+valid_menu_option(1).
+valid_menu_option(2).
+valid_menu_option(3).
+valid_menu_option(4).
+valid_menu_option(5).
+valid_menu_option(6).
+
+/*
   Switch statement definition
   For easier usability of the display_menu function
   Source - https://stackoverflow.com/questions/35764778/switch-statements-in-prolog
@@ -27,7 +58,7 @@ switch(X, [Val:Goal|Cases]) :-
     ).
 
 /*
-Main function to run the program
+* Main function to run the program
 */
 run_bot :-
     writeln("\n********************************************"),
@@ -35,7 +66,7 @@ run_bot :-
     writeln("********************************************"),
     writeln("This is an experimental chatbot to give you useful advice for COVID, and check if your symptoms indicate possible virus contact."),
     writeln("Created by - Harshdip Singh Deogan for COMP4400"),
-    writeln("NOTE: Please make sure to add an period (.) after any input you type."),
+    writeln("NOTE: Please make sure to add a period (.) after any input you type."),
     display_menu.
 
 
@@ -47,40 +78,56 @@ display_menu :-
     writeln("\n\n"),
     writeln("What would you like to do? Type a number from this list: \n"),
     writeln("1. Show COVID-19 Symptoms    2. Enter a symptom to check if you have COVID-19"),
-    writeln("3. Quarantine advice         4. Testing advice"),
+    writeln("3. Quarantine advice         4. Should I get tested?"),
     writeln("5. Vaccine info              6. Exit\n"),
     read(Menu_input),
-    switch(Menu_input, [
-        1 : writeln("lol"),
-        2 : (
-            check_if_covid,
-            display_menu
-        ),
-        3 : (
-            covid_quarantine_advice,
-            display_menu
-        ),
-        4 : (
-            covid_testing_advice,
-            display_menu
-        ),
-        5 : (
-            covid_vaccine_info,
-            display_menu
-        ),
-        6 : (
-            writeln("Exiting... Don\'t forget to practice safe distancing and wear a mask!!!")
+    (
+        valid_menu_option(Menu_input)
+        -> (
+           switch(Menu_input, [
+                1 : writeln("lol"),
+                2 : (
+                    check_if_covid,
+                    display_menu
+                ),
+                3 : (
+                    covid_quarantine_advice,
+                    display_menu
+                ),
+                4 : (
+                    covid_testing_advice,
+                    display_menu
+                ),
+                5 : (
+                    covid_vaccine_info,
+                    display_menu
+                ),
+                6 : (
+                    sleep(0.2),
+                    writeln("Exiting... Don\'t forget to practice safe distancing and wear a mask!!!"),
+                    sleep(0.5)
+                )
+            ])
         )
-    ]).
+        ; (
+            sleep(0.5),
+            writeln("Sorry, I didn\'t recognize that response. Let\'s try again..."),
+            sleep(1),
+            display_menu
+        )
+    ).
+    
 
 check_if_covid :-
+    sleep(0.5),
     writeln("Type out a symptom..."),
     read(Input),
     (
         symptom(Input)
         -> (
             writeln("According to WHO, this symptom can indicate possible contact with the COVID-19 virus."),
-            writeln("You should quarantine immediately. I encourage you to type 1 again to check if you have any other symptoms. Otherwise, type 2 to return to main menu."),
+            sleep(0.5),
+            writeln("\nYou should quarantine immediately. I encourage you to type 1 again to check if you have any other symptoms. Otherwise, type 2 to return to main menu."),
             read(NextInput),
             (
                 NextInput = 1
@@ -89,47 +136,92 @@ check_if_covid :-
             )
         )
         ; (
+            writeln("Hmm..."),
+            sleep(1),
             writeln("This is not a known COVID-19 symptom as of yet.\n"),
+            sleep(1),
             writeln("If you think you made a typo or would like to try again with another symptom, type 1, otherwise type 2 to quit to main menu.\n"),
             read(NextInput),
             (
                 NextInput = 1
                 -> check_if_covid
-                ; writeln("Returning to main menu...")
-            )
-        )
-    ).
-
-covid_quarantine_advice :-
-    writeln("lol stay home what else").
-
-covid_testing_advice :-
-    writeln("I can help you decide if you are wondering if you should get tested for COVID-19."),
-    writeln("Those having symptoms of COVID-19 should separate from others and get tested as soon as possible."),
-    writeln("Have you checked your symptoms and if they indicate that you might have COVID? (yes/no)"),
-    read(Symptom),
-    (
-        Symptom = yes
-        -> (
-            writeln("You should get tested as soon as possible. Make sure to practice safe distancing and quarentine immediately on any doubt!"),
-            writeln("Returning to main menu...")
-        )
-        ; (
-            writeln("Have you been in close contact with a COVID positive person? (yes/no)"),
-            read(Contact),
-            (
-                Contact = yes
-                -> writeln("You should get tested and quarantine as soon as possible.")
                 ; (
-                    writeln("Looks like there isn\'t a immediate need to get tested."),
-                    writeln("However, if you still think you should get tested, please do it. Make sure to practice safe distancing and quarentine immediately on any doubt!"),
-                    writeln("Returning to main menu...")
+                    writeln("Returning to main menu..."),
+                    sleep(1)
                 )
             )
         )
     ).
 
-covid_vaccine_info :-
-    writeln("lmao").
-
+covid_quarantine_advice :-
+    writeln("The Government of Canada has put out detailed information regarding COVID-19."),
+    writeln("Are you recently arriving in Canada from abroad?"),
+    read(Travel),
+    (
+        valid_response(Travel)
+        -> (
+            positive_response(Travel)
+            -> (
+                writeln("The Government of Canada has put in place emergency measures to slow the introduction and spread of COVID-19 in Canada. You MUST QUARANTINE for 14 days, provide contact information and monitor yourself for signs and symptoms as required by the Minimizing the Risk of Exposure to COVID-19 in Canada Order (Mandatory Isolation)."),
+                writeln(" You are encouraged to read more information at https://www.canada.ca/en/public-health/services/publications/diseases-conditions/2019-novel-coronavirus-information-sheet.html")
+            )
+            ; (
+                writeln("Okay, then you should follow the general advice, try to stay indoors and only go outside with proper protetcion!"),
+                writeln("Returning to main menu..."),
+                sleep(1.5)
+            )
+        )
+        ; (
+            writeln("Sorry, I didn\'t recognize that response. Let\'s try again."),
+            sleep(0.4),
+            covid_quarantine_advice
+        )
+    ).
     
+
+covid_testing_advice :-
+    writeln("I can help you decide if you are wondering if you should get tested for COVID-19."),
+    sleep(0.8),
+    writeln("Those having symptoms of COVID-19 should separate from others and get tested as soon as possible."),
+    sleep(0.8),
+    writeln("Have you checked your symptoms and if they indicate that you might have COVID? (yes/no)"),
+    read(Symptom),
+    (
+        valid_response(Symptom)
+        -> (
+            positive_response(Symptom)
+            -> (
+                sleep(0.3),
+                writeln("\nYou should get tested as soon as possible. Make sure to practice safe distancing and quarentine immediately on any doubt!"),
+                writeln("Returning to main menu..."),
+                sleep(1)
+            )
+            ; (
+                writeln("Have you been in close contact with a COVID positive person? (yes/no)"),
+                read(Contact),
+                (
+                    positive_response(Contact)
+                    -> (
+                        writeln("You should get tested and quarantine as soon as possible."),
+                        writeln("Returning to main menu..."),
+                        sleep(1)
+                    )
+                    ; (
+                        writeln("Looks like there isn\'t a immediate need to get tested."),
+                        sleep(1),
+                        writeln("However, if you still think you should get tested, please do it. Make sure to practice safe distancing and quarentine immediately on any doubt!"),
+                        writeln("Returning to main menu..."),
+                        sleep(1)
+                    )
+                )
+            )
+        )
+        ; (
+            writeln("Sorry, I didn\'t recognize that input. Let\'s try again..."),
+            sleep(0.5),
+            covid_testing_advice
+        )
+    ).
+
+covid_vaccine_info :-
+    writeln("lol you wish").
